0a21d5c728743209f178bf8b9523f4e4d4a21e2f
Vijay Saayi Ravinuthala <visaayir@microsoft.com>
2020-01-21 00:04:41 +0530

    Commit 02 : 20/01/20 - JS file edited

--- Scripts/UIElements.js
+++ Scripts/UIElements.js
@@ -1,28 +1,42 @@
-﻿
-function validateTextBoxes(id) {
+﻿function validateAllTextBoxes() {
+    var txtBoxes = ["maxConcurrentRequests", "maxRequests", "requestInterval"];
+    for (var index in txtBoxes) {
+        if (!validateTextBox(txtBoxes[index]))
+            break;
+        else
+            $("#submit_button").attr("disabled", false);
+    }
+
+    
+
+    
+}
+
+
+function validateTextBox(id) {
 
 
     if (isNaN($("#" + id)[0].value) || Number($("#" + id)[0].value) == 0) {
         alert(id + "should be a number between 1 and 4294967295 ");
         $("#submit_button").attr("disabled", true);
+        return false;
     }
-    else {
-        $("#submit_button").attr("disabled", false);
-    }
+    else
+        return true;
 
 }
 
 $(document).ready(function ()
 {
     $("#cb_enableLogging")[0].checked ? $("#denyAction").attr("disabled", true) : $("#denyAction").attr("disabled", false);
-    $("#maxConcurrentRequests")[0].addEventListener('change', function () {
-        validateTextBoxes('maxConcurrentRequests');
+    $("#maxConcurrentRequests")[0].addEventListener('keyup', function () {
+        validateAllTextBoxes()
     });
-    $("#maxRequests")[0].addEventListener('change', function () {
-        validateTextBoxes('maxRequests');
+    $("#maxRequests")[0].addEventListener('keyup', function () {
+        validateAllTextBoxes()
     });
-    $("#requestInterval")[0].addEventListener('change', function () {
-        validateTextBoxes('requestInterval');
+    $("#requestInterval")[0].addEventListener('keyup', function () {
+        validateAllTextBoxes()
     });
     
     
